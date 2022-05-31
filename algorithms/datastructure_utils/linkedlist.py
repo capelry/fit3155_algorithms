ea20378528072fc816d074de2da30632c7d958c5
John Requizo <jereq1@student.monash.edu>
2022-06-01 01:06:30 +1000

    Add read only property tail and head. Add append method

--- algorithms/datastructure_utils/linkedlist.py
+++ algorithms/datastructure_utils/linkedlist.py
@@ -1,5 +1,53 @@
+from __future__ import annotations
+from .node import Node
+from typing import Any
+
+class SinglyLinkedList:
+    def __init__(self) -> SinglyLinkedList:
+        self.__head = None
+        self.__tail = None
+        self.__count = 0
+
+        
+    @property
+    def head(self) -> Node | None:
+        """ Get the Node pointed by the head pointer.
+
+        Returns:
+            Node | None: The node pointed by the head pointer.
+        """
+        return self.__head
 
 
-class LinkedList:
-    def __init__(self):
-        pass
+    @property
+    def tail(self) -> Node | None:
+        """ Get the Node pointed by the tail pointer.
+
+        Returns:
+            Node | None: The node pointed by the tail pointer.
+        """
+        return self.__tail
+
+        
+    def __len__(self) -> int:
+        return self.__count
+
+
+    def append(self, value:Any) -> None:
+        """ Append a value into the Singly Linked List.
+
+        Args:
+            value (Any): A value to append into the Singly Linked List.
+        """
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            new_node.right = None
+            self.head = new_node
+            self.tail = new_node
+            return
+        
+        prev_node = self.tail
+        prev_node.right = new_node
+        new_node.right = None
+        self.tail = new_node
+        self.__count += 1
