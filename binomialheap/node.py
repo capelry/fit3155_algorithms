f089893c0e3105e84a05e701d7a74b296d7b9511
John Requizo <jereq1@student.monash.edu>
2022-05-31 22:03:05 +1000

    Add operators for Node Class

--- binomialheap/node.py
+++ binomialheap/node.py
@@ -18,5 +18,29 @@ class Node:
         self.parent = parent
 
         
+    def __eq__(self, other):
+        return self.key == other.key
+
+        
+    def __ne__(self, other):
+        return self.key != other.key
+
+    
+    def __lt__(self, other):
+        return self.key < other.key
+
+        
+    def __le__(self, other):
+        return self.key <= other.key
+
+
+    def __gt__(self, other):
+        return self.key > other.key
+
+        
+    def __ge__(self, other):
+        return self.key >= other.key
+
+        
     def __repr__(self):
         return f"Node (key={self.key},degree={self.degree})"
