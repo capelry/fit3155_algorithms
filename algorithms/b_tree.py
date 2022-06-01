44658f39dd90ec4a22321ce5dff9318ad02b98b6
John Requizo <jereq1@student.monash.edu>
2022-06-02 00:47:03 +1000

    Add instance variables

--- algorithms/b_tree.py
+++ algorithms/b_tree.py
@@ -10,4 +10,8 @@ class BTree:
     """ Generalised B Tree Class implementation
     """
     def __int__(self, degree:int) -> BTree:
-        pass
+        if degree < 2:
+            raise ValueError("Degree of a B Tree cannot be less than zero")
+        self.l_bound = degree - 1
+        self.u_bound = 2 * degree - 1
+        self.node_list = []
