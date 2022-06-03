a74dcaaa6b3fec9e40b9852dd707a7a77104eda0
capelry <capelry_britto@hotmail.com>
2022-06-04 07:11:19 +1000

    do it baby, delete everything

---
+++ algorithms_cam/Tableau_LP.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def tableua(obj_coeff, rest_coeffs):
+    '''
+    inputs
+    obj_coeff: list of coefficients in objective equation
+    rest_coeffs: list of list containing restraint coeffs
+    
+    output: optimum values
+    '''
+
+    # create calculation table
+    
---
+++ algorithms_cam/test.py
@@ -0,0 +1,8 @@
+from Tableau_LP import tableua
+
+# z = 6x + 5y
+objective = [6, 5, 1]
+# rest of coeffs
+# x + y <= 5 and 3x + 2y <= 12
+coeffs = [[1, 1, 5], [3, 2, 12]]
+results = tableua(objective, coeffs)
